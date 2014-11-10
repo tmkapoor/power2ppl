2aab035ecf54433cdf813e81f986f624e9c2bd00
Tanay Manohar Kapoor <tmkapoor@gmail.com>
2014-11-10 13:43:16 +0000

    added graphing

---
+++ api/controllers/GraphingController.js
@@ -0,0 +1,13 @@
+/**
+ * GraphingController
+ *
+ * @description :: Server-side logic for managing graphings
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	index: function(req, res, next){
+		res.view();
+	}
+};
+
