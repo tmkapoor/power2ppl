7f7018560a3e197e6a88cec8e82bb9897a78e92a
Tanay Manohar Kapoor <tmkapoor@gmail.com>
2014-11-18 10:50:38 +0000

    stage4 - photo proto up

---
+++ api/controllers/Stage4Controller.js
@@ -0,0 +1,13 @@
+/**
+ * Stage4Controller
+ *
+ * @description :: Server-side logic for managing stage4s
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	index: function(req, res, next){
+		res.view();
+	}
+};
+
--- assets/js/dependencies/main.js
+++ assets/js/dependencies/main.js
@@ -1,6 +1,42 @@
+function recalc(){
+	var holder = $(".holder");
+	var pos = holder.position();
+	var w = holder.width();
+	var h = holder.height();
+	var bigMap = $(".floater");
+	bigMap.show();
+	bigMap.css({ 
+	    position: "absolute",
+	    top: pos.top,
+	    left: pos.left,
+	    height: h,
+	    width: w,
+	});
+}
+
  $(document).ready(function(){
 
  	var dataSelected = 0;
+
+ 	$("#graph-bg").load(function(e){
+ 		//$(".graph-instruction").load(function(e){
+ 			var instr = $(".graph-instruction");
+ 			var graph = $("#graph-bg");
+
+ 			var graphPos = graph.position();
+ 			var graphWidth = graph.width();
+ 			var graphHeight = graph.height();
+
+ 			instr.css({
+ 				position: "absolute",
+ 				top: graphPos.top,
+ 				left: graph.left,
+ 				height: graphHeight,
+ 				width: graphWidth,
+ 				display: "block",
+ 			});
+ 		//});
+ 	});
 
  	$(".clickable").click(function(e){
  		if(!($(this).hasClass('active-card'))){
@@ -9,32 +45,29 @@
  		else{
  			dataSelected--;
  		}
+ 		
  		$(this).toggleClass('active-card');
- 		if($("#graph").is(":visible") ){
- 			$("#graph").hide("slow");
+
+ 		if(dataSelected == 0){
+ 			$(".graph-instruction").show("slow");
+ 			$("#graph-reading").slideToggle("slow", function() {
+				//$("html, body").animate({ scrollTop: $(document).height() }, "slow");
+	  			return false;
+			});
+ 		}
+ 		else{
+ 			$(".graph-instruction").hide("slow");
  		}
  		if(dataSelected > 0){
- 			$("#graph").show( "slow", function() {
-				$("html, body").animate({ scrollTop: $(document).height() }, "slow");
+ 			$("#graph-reading").slideToggle("slow", function() {
+				//$("html, body").animate({ scrollTop: $(document).height() }, "slow");
 	  			return false;
 			});
  		}
 	});
 
 	$(".map").click(function(e) {
-		var holder = $(".holder");
-		var pos = holder.position();
-		var w = holder.width();
-		var h = holder.height();
-		var bigMap = $(".floater");
-		bigMap.show();
-		bigMap.css({ 
-		    position: "absolute",
-		    top: pos.top,
-		    left: pos.left,
-		    height: h,
-		    width: w,
-		});
+		recalc();
 	});
 
 	$(".floater").click(function(e){
