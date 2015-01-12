d5dfd756305338b06dc408fe2b978ddbe734b783
Tanay Manohar Kapoor <tmkapoor@gmail.com>
2015-01-12 14:23:41 +0000

    fixed stage3old

---
+++ api/controllers/Stage3oldController.js
@@ -0,0 +1,13 @@
+/**
+ * Stage3oldController
+ *
+ * @description :: Server-side logic for managing stage3olds
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	index: function(req, res, next){
+  	res.view();
+  },
+};
+
